488ed96b4bf1f4ed7eeeffbf02a19d417ca8a8d4
gpioli <gpioli182@gmail.com>
2022-11-06 12:22:36 -0300

    Data types

---
+++ 003_data_types.py
@@ -0,0 +1,24 @@
+# Remember python y a dynamic typing language, which means we don't have to
+# declare the var type
+
+# string
+my_name = "Mike"
+my_name = 'Louis'
+
+print('my_name =>', my_name)
+
+# int
+my_age = 12
+print('my_age =>', my_age)
+
+# boolean
+is_single = True
+is_single2 = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+# ----Starts program execution---- #
+
+# inputs: input function always takes as an input String type
+my_age = input('What"s your age? ')
+print('my_age => ', my_age)

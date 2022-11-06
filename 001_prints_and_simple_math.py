b406a2bb20605d659d0ffcfb91cc4fb870995df1
gpioli <gpioli182@gmail.com>
2022-11-06 12:09:27 -0300

    Starting with the python practice

---
+++ 001_prints_and_simple_math.py
@@ -0,0 +1,20 @@
+### to run this file from terminal run:
+# "python 001_prints_and_simple_math.py"
+
+print("Hi, this is python")
+
+# This is a comment
+'''This is also a comment'''
+"""And this one also is a comment"""
+
+## Some simple calculation
+
+# Summing
+print(12 + 5)
+print(12 - 5)
+
+# Multiplication
+print(2 * 3)
+
+# Division
+print(12 / 4)
---
+++ 002_vars.py
@@ -0,0 +1,23 @@
+print("Hi, vars")
+
+# Saving Strings
+my_name = "Gaston"
+print(my_name)
+
+# Saving numbers
+my_age = "18"
+print(my_age)
+
+# Re-assigning var
+my_name = "Milton"
+print("Here the var value changed to: ", my_name)
+
+print("")
+print("--------------Starting program--------------")
+
+# Receiving inputs with the function "input"
+your_name = input("What's your name? ")
+print("Hi,", your_name)
+
+your_age = input("What's your age? ")
+print("You are", your_age,"years old.")

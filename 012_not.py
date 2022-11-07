c85477a88ff5279597e45d40fb3b0d63a8fead15
gpioli <gpioli182@gmail.com>
2022-11-07 00:24:37 -0300

    working with not

---
+++ 012_not.py
@@ -0,0 +1,12 @@
+# working with not
+
+print(not True)
+print(not False)
+
+# not
+print('or')
+print('not True and True =>', not (True or True))
+print('not True and False =>', not (True or False))
+print('not False and True =>', not (False or True))
+print('not False and False =>', not (False or False))
+

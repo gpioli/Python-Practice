23fc502183bf7747ae51df5882de37446130775e
gpioli <gpioli182@gmail.com>
2022-11-06 23:57:11 -0300

    working with comparators

---
+++ 009_comparators.py
@@ -0,0 +1,26 @@
+# greater than >
+
+print(7 > 3) # returns True
+print(3 > 7) # returns False
+print(7 > 7) # returns False
+
+# Less than <
+print(5 < 6) # returns True
+print(6 < 5) # returns False
+
+print("")
+# greater or equal than (>=) and less or equal than (<=)
+print(1<=2) # prints True
+print(1<=1) # prints True
+print(1 >= 2) # prints False
+
+print("")
+# equal to (==) and different to:
+print(6==6) #prints True
+print(5!=6) #prints True
+
+# comparing strings:
+print("Comparing strings")
+print("Apple" == "Apple") # returns True
+print("Apple" == "apple") # returns False
+print("1" == 1) # returns False (different data types)

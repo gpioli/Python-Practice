036fe917b149cc16d51a4c09502aa9fb3198e669
gpioli <gpioli182@gmail.com>
2022-11-07 00:03:43 -0300

    Working with floats and comparing them

---
+++ 010_float.py
@@ -0,0 +1,19 @@
+
+x = 3.3
+print(x)
+y = 1.1 + 2.2
+print(y)
+
+print(x == y) # This returns False... so, how do we compare floats?
+
+# format allows us to re format the number avoiding all the decimal precision
+y_str = format(y, ".2g")
+print('str => ', y_str)
+print(y_str == str(x)) # This is one way
+# But this is not a mathematical way...
+
+print('*' * 10)
+
+print(y,x)
+tolerance = 0.00001
+print(abs(x - y) < tolerance)

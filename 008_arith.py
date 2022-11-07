151539f8a65eaacb178385b0f87b31c9099c9857
gpioli <gpioli182@gmail.com>
2022-11-06 23:07:14 -0300

    Working with arithmethic operations

---
+++ 008_arith.py
@@ -0,0 +1,19 @@
+print(10 + 10)
+print(25 - 5)
+print(10 * 2)
+print(40 / 2)
+
+print('Hi' + ' world')
+print('Hi' * 3) # prints HIHIHI
+
+print(10 % 2) # Residual of the division (module operator)
+print(10 // 3) # We only kept the 'whole' number part | This prints 3
+print(2 ** 3 ) # 2 to the power of 3
+
+# order of the arithmetic operations: PEMDAS
+# parenthesis, exponents, multiplication, division, adition, sustraction
+
+print(2 ** 3 + 3 - 7 / 1 // 4)
+# (8 + 3 - (7 // 4))
+# 8 + 3 - 1 => 10
+

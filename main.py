740de46b8b22004448fa3261f2585d6626c707e4
gpioli <gpioli182@gmail.com>
2022-11-13 13:34:41 -0300

    Modifications to README

--- main.py
+++ main.py
@@ -5,34 +5,65 @@ import random
 
 options = ("rock", "papper", "scissors")
 
+rounds = 1
+computer_wins = 0
+user_wins = 0
+
+print("*" * 40)
 print("Welcome to rock, papper, scissors game!")
-user_option = input('Please, write your choice: rock, paper, scissors => ').lower()
+print("*" * 40)
+print("")
 
-if not user_option in options:
-    print("You entered an invalid option")
+while True:
+
+    print("*" * 10)
+    print("Round: ", rounds)
+    print("*" * 10)
+    print("User wins: ", user_wins)
+    print("Computer wins: ", computer_wins)
 
 
-computer_option = random.choice(options)
+    user_option = input('Please, write your choice: rock, paper, scissors => ').lower()
 
-print("User opton =>", user_option)
-print("Computer option=>", computer_option)
+    if not user_option in options:
+        print("You entered an invalid option")
+        continue
 
-if user_option == computer_option:
-    print("It's a tie!")
-elif user_option == 'rock':
-    if computer_option == 'scissors':
-        print("Rock wins over scissors!")
-        print("You win!")
-    else:
-        print("Papper wins over rock!")
-        print("Computer wins!")
-elif user_option == 'papper':
-    if computer_option == 'rock':
-        print('Papper wins over rock')
+    computer_option = random.choice(options)
+
+    print("User opton =>", user_option)
+    print("Computer option=>", computer_option)
+
+    if user_option == computer_option:
+        print("It's a tie!")
+    elif user_option == 'rock':
+        if computer_option == 'scissors':
+            print("Rock wins over scissors!")
+            print("You win!")
+            user_wins += 1
+        else:
+            print("Papper wins over rock!")
+            print("Computer wins!")
+            computer_wins += 1
+    elif user_option == 'papper':
+        if computer_option == 'rock':
+            print('Papper wins over rock')
+            print('You win!')
+            user_wins += 1
+        else:
+            print('Scissors win over papper!')
+            print('Computer wins!')
+            computer_wins += 1
+    elif user_option == 'scissors':
+        print('Scissors wins over papper!')
         print('You win!')
-    else:
-        print('Scissors win over papper!')
-        print('Computer wins!')
-elif user_option == 'scissors':
-    print('Scissors wins over papper!')
-    print('You win!')
+        user_wins += 1
+
+    if computer_wins == 2:
+        print("Computer wins the match!")
+        break
+    elif user_wins == 2:
+        print("User wins the match! Congratulations!")
+        break
+
+    rounds += 1

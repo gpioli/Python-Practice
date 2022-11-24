fe84dba91c0a0d2928d11667d0438163a7a8626c
Gaston Pioli <milton.pioli@despegar.com>
2022-11-24 13:41:50 -0300

    Working with functions

---
+++ 2-Python-Basics-II/008_functions.py
@@ -0,0 +1,31 @@
+# https://www.w3schools.com/python/python_functions.asp
+
+# we have already worked with functions:
+print("Hello")
+
+# creating functions
+def my_print():
+    print("This is my print")
+
+my_print()
+
+
+# Creating a function that receives parameters:
+def my_print_with_params(text):
+    print("Hello,", text)
+
+my_print_with_params("Gaston")
+
+#
+a = 10
+b = 90
+
+# this doesn't scale
+c = a + b
+
+
+def suma(x, y):
+    print(x + y)  # this functions has two responsibilities: summing and printing
+
+
+suma(10, 5)

4bd802593e0ff0c43f7ea0a9617804192ce11c94
Gaston Pioli <milton.pioli@despegar.com>
2022-11-25 02:16:18 -0300

    working with Higher order functions

---
+++ 2-Python-Basics-II/013_higher_order_functions.py
@@ -0,0 +1,21 @@
+#
+
+
+def increment(x):
+    return x + 1
+
+
+def high_ord_func(x, func):
+    return x + func(x)
+
+
+result = high_ord_func(2, increment)
+# 2 + (2 + 1)
+print(result)
+
+
+# defining higher order functions with lambdas
+increment_v2 = lambda x: x + 1
+high_ord_func_v2 = lambda x, func: x + func(x)
+result_2 = high_ord_func_v2(2, increment_v2)
+print(result_2)

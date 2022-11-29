614ed1048edb70ebf5dbce01c0c47da8c4e10c83
Gaston Pioli <milton.pioli@despegar.com>
2022-11-29 18:06:17 -0300

    doing some refactoring

--- 2-Python-Basics-II/024_write.py
+++ 2-Python-Basics-II/024_write.py
@@ -1,22 +1,22 @@
 # This will not work, as by defect we work with read permits:
-# with open("./23_text_file.txt", "r") as file:
+# with open("./023_text_file.txt", "r") as file:
 #     for line in file:
 #         print(line)
 #     file.write("new things in this file")
 
 # with r+ permit we can add new content
-with open("./23_text_file.txt", "r+") as file:
+with open("./023_text_file.txt", "r+") as file:
     for line in file:
         print(line)
     file.write("\n new things in this file.")
     file.write("\n and some more things.")
 
-with open("./23_text_file.txt") as file:
+with open("./023_text_file.txt") as file:
     for line in file:
         print(line)
 
 # with w+ permit we can read the file, but all the content in the file will be overwritten:
-with open("./23_text_file.txt", "w+") as file:
+with open("./023_text_file.txt", "w+") as file:
     for line in file:
         print(line)
     file.write("\n new things in this file.")
--- 2-Python-Basics-II/files.py
+++ 2-Python-Basics-II/files.py
@@ -1,7 +1,7 @@
 # We are going to work with files
 # https://realpython.com/working-with-files-in-python/
 
-file = open("./23_text_file.txt")
+file = open("023_text_file.txt")
 # This will read all the text in the text file | If it's a big file, the memory consumption will be high
 # print(file.read())
 
@@ -14,14 +14,14 @@ print(file.readline())
 file.close()
 
 # Another way: | This has the best of both worlds: low memory consumption and knows when to stop reading
-file = open("./23_text_file.txt")
+file = open("023_text_file.txt")
 for line in file:
     print(line)
 file.close()
 
 # Something very important is to close the opened file once finished the operation | This can be done with the following
 # instruction: (opens the file and closes it once finished doing the requested operations
-with open("./23_text_file.txt") as file:
+with open("023_text_file.txt") as file:
     for line in file:
         print(line)
 
